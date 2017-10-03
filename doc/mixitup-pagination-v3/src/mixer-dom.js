3edbbcb9c3495da92fc5def208a74afee4dfa69a
panggws <pangapang.ws@hotmail.com>
2017-10-03 16:24:30 +0700

    remove files

--- doc/mixitup-pagination-v3/src/mixer-dom.js
+++
@@ -1,6 +0,0 @@
-/* global mixitup */
-
-mixitup.MixerDom.registerAction('afterConstruct', 'pagination', function() {
-    this.pageList  = null;
-    this.pageStats = null;
-});
--- doc/portfolio/ThemePicasso Themes _ Item   Stain-wp_files/a_data/royal_preload_config.js
+++
@@ -1,6 +0,0 @@
-Royal_Preloader.config({
-    mode           : 'progress',
-    background     : '#f5f5f5',
-    showProgress   : true,
-    showPercentage : true
-});
